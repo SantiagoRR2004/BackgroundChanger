921f861fa461d58537c5ff993e6adfddea2c3a7d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-23 19:11:13 +0200

    Works with crontab

--- Crontabs/BackgroundChanger.py
+++ Crontabs/BackgroundChanger.py
@@ -2,6 +2,7 @@ import platform
 import subprocess
 import ctypes
 import os
+from Modules import Utils
 
 def set_wallpaper(image_path: str) -> None:
     """
@@ -26,19 +27,28 @@ def set_wallpaper(image_path: str) -> None:
         subprocess.run(["osascript", "-e", script])
     elif system == "Linux":
         desktop_env = subprocess.run(["echo $XDG_CURRENT_DESKTOP"], shell=True, capture_output=True, text=True).stdout.strip()
-        if "GNOME" in desktop_env or "Unity" in desktop_env:
-            if os.path.exists(image_path):
-                subprocess.run(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"file://{image_path}"])
-            else:
-                subprocess.run(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"{image_path}"])
-        else:
+        if "GNOME" not in desktop_env and "Unity" not in desktop_env:
             print(f"Unsupported Linux desktop environment {desktop_env}")
+
+
+        command = """export DISPLAY=":0"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+gsettings set org.gnome.desktop.background picture-uri"""
+
+        if os.path.exists(image_path):
+            command += f" file://{image_path}"
+        else:
+            command += f" {image_path}"
+        command += "\n gsettings set org.gnome.desktop.background picture-options scaled"
+        Utils.runTerminal(command)
+
+
     else:
         print("Unsupported operating system")
 
 
 if __name__ == "__main__":
-    import time
+    import random
     backgrounds = [
         "https://www.woodus.com/den/gallery/graphics/dq9ds/wallpaper/monsters.jpg",
         "https://www.spriters-resource.com/resources/sheets/45/47529.png?updated=1460959095",
@@ -46,9 +56,12 @@ if __name__ == "__main__":
         "https://steamuserimages-a.akamaihd.net/ugc/2058741034012526512/379E6434B473E7BE31C50525EB946D4212A8C8B3/",
         "https://www.capcom-games.com/ghosttrick/assets/images/video/video_img1_en.jpg",
         "https://images7.alphacoders.com/749/749807.png",
-        "https://ubuntu.com/wp-content/uploads/c919/noble1.png"
+        "https://ubuntu.com/wp-content/uploads/c919/noble1.png",
+        "https://static.wikia.nocookie.net/eldenring/images/3/38/ER_Icon_Remembrance_of_the_Grafted.png/revision/latest?cb=20220412231514",
+        "https://static.wikia.nocookie.net/eldenring/images/6/65/ER_Icon_Remembrance_of_the_Full_Moon_Queen.png/revision/latest?cb=20220412231510",
+        "https://static.wikia.nocookie.net/eldenring/images/3/3d/ER_Icon_Remembrance_of_the_Starscourge.png/revision/latest/scale-to-width-down/1000?cb=20220412231542",
+        "https://static.wikia.nocookie.net/eldenring/images/2/2c/ER_Icon_Remembrance_of_the_Regal_Ancestor.png/revision/latest?cb=20220412231535",
     ]
 
-    for background in backgrounds:
-        set_wallpaper(background)
-        time.sleep(2)
+
+    set_wallpaper(random.choice(backgrounds))
